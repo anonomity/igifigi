77ad88d241fa220260b2de7f707c04f6c7000eb4
Igifigi <ignacyswiderski@wp.pl>
2020-03-25 12:57:30 +0100

    Added RAINBOW!!!

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,6 +8,21 @@ class blog extends Component {
         name: "Type your name",
         msg: "Type message",
         time: "Type the current time (HH:MM DD.MM.YYYY)",
+        posts: {
+             0: {    
+                id: 1,
+                 name: "Ig",
+                 msg: "dsafasdf",
+                 time: "asdfa"
+             },
+             1: {
+                  id: 2,
+                  name: "Ig",
+                   msg: "dsafasdf",
+                  time: "asdfa"
+              },
+          }
+        
     }
     resetBlog = () => {
         this.setState({
@@ -33,10 +48,20 @@ class blog extends Component {
     }
 
     render(){
+        //  let Bposts = null;
+        //  Bposts = this.state.posts.map(post => {
+        //      return(
+        //          <div><BlogPost name = {post.name} msg = {post.msg} time = {post.time}/></div>
+                
+        //      )
+        // })
+        let Title = <h1>Hello!</h1>
         return(
             <div>
-                <h1>blog</h1>
-                <BlogPost name="Igi" msg="Hello!" time="19:00 06.03.2007" />
+
+                {Title}
+                {/* {Bposts} */}
+                 <BlogPost name={this.state.posts[0].name} msg="Hello!" time="19:00 06.03.2007" />
                 <BlogPost name="Jackie" msg="plz no c++
                 :-)" time="06:00 29.02.2020"/>
                 <BlogPost name="Magda" msg="Welcome to Catty Corner Clubhouse" />
@@ -62,7 +87,7 @@ class blog extends Component {
                 gklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjsk
                 ndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgs" time="16:232323123 24.33.2020"/>
                 <BlogPost name="Julia" msg="i like billie eyelash" time="16:00 24.33.2020"/>
-                <BlogPost name={this.state.name} msg={this.state.msg} time={this.state.time}/>
+                <BlogPost name={this.state.name} msg={this.state.msg} time={this.state.time}/> 
                 <div className="post">
                 <input className="inputs" type="sumbit" value = {this.state.name} onChange = {this.changeNameState}></input>
                 <input className="inputs" type="sumbit" value = {this.state.msg} onChange = {this.changeMsgState}></input>
@@ -72,6 +97,7 @@ class blog extends Component {
                 
             </div>
         );
+        
     }
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,11 +6,25 @@ class home extends Component {
     // function handleClick(){
     //     console.log("test");
     // }
+    state = {
+        isPressed: false
+    }
+    showTitle = () => {
+        this.setState({
+            isPressed: true
+        })
+    }
+    
     render(){
-   
+        let X = null;
+        if(this.state.isPressed){
+            X = <h1 style={{fontFamily: "Titillium Web"}}>WELCOME!</h1>
+        }
         return(
             <div className="home">
+                {X}
                 <h1>home</h1>
+                <button onClick={this.showTitle}>CLICK ME! (please) NOW!!!</button>
                 {/* <button onClick={()=>{this.handleClick}}>click me!</button> */}
             </div>
         );
