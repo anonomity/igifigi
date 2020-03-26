f458ddf4770a4765c7bd8c7e2a5eaba40dad799c
Jackie <jlauravelez@gmail.com>
2020-03-26 06:53:55 +0100

    Mapping blog posts and rendering it with a button

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,104 +1,97 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import BlogPost from "../components/BlogPost";
-import '../css/blogPost.css';
+import "../css/blogPost.css";
 import EmptyBlogPost from "../components/EmptyBlogPost";
 
 class blog extends Component {
-    state = {
-        name: "Type your name",
-        msg: "Type message",
-        time: "Type the current time (HH:MM DD.MM.YYYY)",
-        posts: {
-             0: {    
-                id: 1,
-                 name: "Ig",
-                 msg: "dsafasdf",
-                 time: "asdfa"
-             },
-             1: {
-                  id: 2,
-                  name: "Ig",
-                   msg: "dsafasdf",
-                  time: "asdfa"
-              },
-          }
-        
-    }
-    resetBlog = () => {
-        this.setState({
-            name: "Type your name",
-            msg: "Type message",
-            time: "Type the current time (HH:MM DD.MM.YYYY)",
-        })
-    }
-    changeNameState = (event) => {
-        this.setState({
-            name : event.target.value
-        })
-    }
-    changeMsgState = (event) => {
-        this.setState({
-            msg : event.target.value
-        })  
-    }
-    changeTimeState = (event) => {
-        this.setState({
-            time : event.target.value
-        })  
+  state = {
+    name: "Type your name",
+    msg: "Type message",
+    time: "Type the current time (HH:MM DD.MM.YYYY)",
+    posting: [
+      {
+        id: 1,
+        name: "Ig",
+        msg: "dsafasdf",
+        time: "asdfa"
+      },
+      {
+        id: 2,
+        name: "Ig",
+        msg: "dsafasdf",
+        time: "asdfa"
+      }
+    ],
+    isRendered: false
+  };
+  resetBlog = () => {
+    this.setState({
+      name: "Type your name",
+      msg: "Type message",
+      time: "Type the current time (HH:MM DD.MM.YYYY)"
+    });
+  };
+  changeNameState = event => {
+    this.setState({
+      name: event.target.value
+    });
+
+  };
+  changeMsgState = event => {
+    this.setState({
+      msg: event.target.value
+    });
+  };
+  changeTimeState = event => {
+    this.setState({
+      time: event.target.value
+    });
+  };
+  renderIt = () => {
+    this.setState({
+      isRendered: true
+    });
+  };
+  render() {
+    let Bposts = null;
+    if (this.state.isRendered) {
+      Bposts = this.state.posting.map(post => {
+        return <BlogPost name={post.name} msg={post.msg} time={post.time} />;
+      });
     }
 
-    render(){
-        //  let Bposts = null;
-        //  Bposts = this.state.posts.map(post => {
-        //      return(
-        //          <div><BlogPost name = {post.name} msg = {post.msg} time = {post.time}/></div>
-                
-        //      )
-        // })
-        let Title = <h1>Hello!</h1>
-        return(
-            <div>
-
-                {Title}
-                {/* {Bposts} */}
-                 <BlogPost name={this.state.posts[0].name} msg="Hello!" time="19:00 06.03.2007" />
-                <BlogPost name="Jackie" msg="plz no c++
-                :-)" time="06:00 29.02.2020"/>
-                <BlogPost name="Magda" msg="Welcome to Catty Corner Clubhouse" />
-                <BlogPost name="Janka" msg="akjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfs
-                nfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsd
-                fhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnkl
-                ajdngklsjdgfjkdfnjskndajdngklsjdgfjkdfnjskndfsnfgnsd
-                kfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjf
-                kaljdgnklajdngklsjdgffsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfn
-                jskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjg
-                sakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkal
-                jdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgf
-                jkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnkljkdfnjskndfsnfgnsdkfjgsakjsdfhs
-                fjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsd
-                kfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkalj
-                dgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkd
-                fnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsa
-                kjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnkl
-                ajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjsknd
-                fsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskl
-                djfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdg
-                fjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdn
-                gklsjdgfjkdfnjskndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjsk
-                ndfsnfgnsdkfjgsakjsdfhskldjfkaljdgnklajdngklsjdgfjkdfnjskndfsnfgnsdkfjgs" time="16:232323123 24.33.2020"/>
-                <BlogPost name="Julia" msg="i like billie eyelash" time="16:00 24.33.2020"/>
-                <BlogPost name={this.state.name} msg={this.state.msg} time={this.state.time}/> 
-                <div className="post">
-                <input className="inputs" type="sumbit" value = {this.state.name} onChange = {this.changeNameState}></input>
-                <input className="inputs" type="sumbit" value = {this.state.msg} onChange = {this.changeMsgState}></input>
-                <input className="inputs" type="sumbit" value = {this.state.time} onChange = {this.changeTimeState}></input>
-                <button onClick={this.resetBlog} className="inputs">RESET</button>
-            </div>
-                
-            </div>
-        );
-        
-    }
+    return (
+      <div style={{ marginTop: "100px" }}>
+        {Bposts}
+        <button onClick={this.renderIt} style={{marginLeft: "auto", marginRight:"auto", display: "block"}}>CLICK ME</button>
+        <div className="post">
+          
+          
+          <input
+            className="inputs"
+            type="sumbit"
+            value={this.state.name}
+            onChange={this.changeNameState}
+          ></input>
+          <input
+            className="inputs"
+            type="sumbit"
+            value={this.state.msg}
+            onChange={this.changeMsgState}
+          ></input>
+          <input
+            className="inputs"
+            type="sumbit"
+            value={this.state.time}
+            onChange={this.changeTimeState}
+          ></input>
+          <button onClick={this.resetBlog} className="inputs">
+            RESET
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default blog;
